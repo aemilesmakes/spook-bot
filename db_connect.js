75a7e5019ce3af81e63a90d5780d8a6e61cc994c
Amy M <amy.miles1@pcc.edu>
2022-05-14 22:05:04 -0700

    db_connect now queries sfc mongodb, returns random spook from collection where seen = false

--- db_connect.js
+++ db_connect.js
@@ -6,25 +6,39 @@ module.exports = async function connectDB() {
     try {
         // Connect to the MongoDB cluster
         await client.connect();
-
-
         console.log("Connected to DB!");
+
         // Make the appropriate DB calls
-        //await  listDatabases(client);
+        await  getSingleSpook(client, {
+            maximumNumberOfResults: 1
+        });
 
     } catch (e) {
         console.error(e);
     } finally {
         await client.close();
+        console.log("Connection closed!");
     }
 }
 
 //main().catch(console.error);
 
-async function listDatabases(client) {
-    let databasesList = await client.db().admin().listDatabases();
+async function getSingleSpook(client, {
+    maximumNumberOfResults = Number.MAX_SAFE_INTEGER} = {}) {
+    const cursor = client.db("spooky_film_club").collection("syllabus").find(
+        {
+            seen: { $eq: false },
+        });
 
-    console.log("Databases:");
-    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+    const results = await cursor.toArray();
+
+    if (results.length > 0) {
+        let random = Math.floor(Math.random() * results.length);
+        console.log(`How about this one: ${results[random].title} (${results[random].year})`);
+    } else {
+        console.log(`No spooks found. :(`);
+    }
 }
+
+
 
