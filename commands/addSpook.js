022d523c125dd2e78540c5ee1e0b0add1b220f23
Amy M <amy.miles1@pcc.edu>
2022-05-23 22:52:04 -0700

    added new DB query for seen spooks, renamed some methods

--- commands/addSpook.js
+++ commands/addSpook.js
@@ -1,7 +1,7 @@
 // Add a spook to the database
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { client } = require('../connect.js');
-const { matchSyllabus } = require("../syllabus");
+const { checkExisting } = require("../syllabus");
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -27,9 +27,10 @@ module.exports = {
         const movieDir = interaction.options.getString('director');
 
         const movie = `${movieTitle} (${movieYear})`;
-        let syllabus = await matchSyllabus();
-        let alreadyExists = !!syllabus.find(spook =>
+        let syllabus = await checkExisting();
+        let alreadyExists = syllabus.find(spook =>
             spook === movie)
+        console.log(alreadyExists);
 
         if (alreadyExists === true) {
             await interaction.reply(`That spook is already on the syllabus!`);
@@ -37,13 +38,13 @@ module.exports = {
             await interaction.reply(`That's not a real year......... I'm not adding that :(`);
         } else {
             //pass inputs to db
-            await client.connect();
+/*            await client.connect();
             await addSpook(client, {
                 title: `${movieTitle}`,
                 year: parseInt(movieYear),
                 director: `${movieDir}`,
                 seen: false
-            });
+            });*/
             await interaction.reply(`New spook added: ${movieTitle} (${movieYear})!`);
         }
         await client.close();
--- syllabus.js
+++ syllabus.js
@@ -1,14 +1,16 @@
 const { client } = require('./connect.js');
+const date = require('date-and-time');
 
 /*
-Query the DB for the whole list of spooks/syllabus
+Query the DB for the whole list of movies in the DB, watched & unwatched (all spooks).
+List is automatically sorted by release year.
  */
 async function getAllSpooks() {
     await client.connect();
 
     const cursor = client.db("spooky_film_club").collection("syllabus").find(
         {
-        });
+        }).sort({ year: 1 });
 
     const results = await cursor.toArray();
 
@@ -21,22 +23,45 @@ async function getAllSpooks() {
 }
 
 /*
-Get an array from full list of spooks that's JUST the movie title + year, for matching purposes.
+Query the DB for ONLY movies that we've watched, and sort those movies by date watched.
+Returns an object array.
+ */
+async function getSeenSpooks() {
+    await client.connect();
+
+    const cursor = client.db("spooky_film_club").collection("syllabus").find(
+        {
+            seen: true
+        }).sort({ date_assigned: 1 });
+
+    const results = await cursor.toArray();
+
+    if (results.length > 0) {
+        return results;
+    } else {
+        console.log(`No spooks found. :(`);
+    }
+    await client.close();
+}
+
+/*
+Take the allSpooks array -> convert it into an array of JUST the movie title + movie year.
+This is for checking against when a new spook is added using the addSpook command.
  */
 
-async function matchSyllabus() {
+async function checkExisting() {
     let syllabus = await getAllSpooks();
     const arraySyllabus = [];
 
     for (let i = 0; i < syllabus.length; i++) {
         arraySyllabus.push(`${syllabus[i].title} (${syllabus[i].year})`);
     }
-    await client.close();
+
     return arraySyllabus;
 }
 
 /*
-Gets the to-watch list as an object array, sorted by year
+Gets the to-watch list as an object array, for querying with getSingleSpook.
  */
 async function getToWatch() {
     let syllabus = await getAllSpooks();
@@ -51,10 +76,27 @@ async function getToWatch() {
 }
 
 /*
-Prints the to-watch list as a string, for printing in Discord
+Converts the object array of watched spooks into a string, so SpookBot can print it in Discord.
+Uses date-and-time package to make the date nicer.
  */
 
-async function stringSyllabus() {
+async function stringSeen() {
+    let seenSpooks = await getSeenSpooks();
+    let stringSeen = "";
+
+    for (let i = 0; i < seenSpooks.length; i++) {
+        let dateWatched = date.parse((seenSpooks[i].date_assigned).substring(0,10),'YYYY-MM-DD');
+        let dateClean = date.format(dateWatched,'MMM DD, YYYY')
+        stringSeen += `${seenSpooks[i].title} (${seenSpooks[i].year}) - ${dateClean}\n`
+    }
+    return stringSeen;
+}
+
+/*
+Converts the to-watch object array into a string, so SpookBot can print it in Discord.
+ */
+
+async function stringToWatch() {
     let syllabus = await getAllSpooks();
     let stringSyllabus = "";
 
@@ -67,10 +109,10 @@ async function stringSyllabus() {
 }
 
 module.exports = {
-    //getSyllabus,
-    //getFullSyllabus,
-    getAllSpooks,
+//    getAllSpooks,
+//    getSeenSpooks,
     getToWatch,
-    matchSyllabus,
-    stringSyllabus
+    checkExisting,
+    stringSeen,
+    stringToWatch
 }
