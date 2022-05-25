6955cd87e67cb6a3350325fe0070d66047e83db1
Amy M <amy.miles1@pcc.edu>
2022-05-24 22:43:48 -0700

    Added "seen" option to /add command.

--- commands/addSpook.js
+++ commands/addSpook.js
@@ -16,6 +16,18 @@ module.exports = {
                 .setDescription('What year did it come out?')
                 .setRequired(true)
         ).addStringOption(option =>
+            option.setName('seen')
+                .setDescription('Have we watched it?')
+                .setRequired(false)
+                .addChoices({
+                    name: 'yes',
+                    value: 'true'
+                })
+                .addChoices({
+                    name: 'no',
+                    value: 'false'
+                })
+        ).addStringOption(option =>
             option.setName('director')
                 .setDescription('Who directed it?')
                 .setRequired(false)
@@ -25,15 +37,26 @@ module.exports = {
         const movieTitle = interaction.options.getString('title');
         const movieYear = interaction.options.getString('year');
         const movieDir = interaction.options.getString('director');
+        const haveWatched = interaction.options.getString('seen');
+        const movieString = `${movieTitle} (${movieYear})`;
 
-        const movie = `${movieTitle} (${movieYear})`;
         let syllabus = await checkExisting();
-        let alreadyExists = syllabus.find(spook =>
-            spook === movie)
-        console.log(alreadyExists);
+        let alreadyExists = !!syllabus.find(spook =>
+            spook === movieString)
+
+        //TEST
+        console.log(movieString);
+        console.log("Already on the syllabus = " + alreadyExists);
+        console.log("Have we watched it? = " + haveWatched);
+
+        //I THINK I NEED A SEPARATE CHECK FUNCTION THAT LOOKS TO SEE IF IT'S ON THE "WATCHED" LIST OVER IN SYLLABUS.JS
 
         if (alreadyExists === true) {
-            await interaction.reply(`That spook is already on the syllabus!`);
+            if (seen === true) {
+                await interaction.reply(`We've already watched that.......`);
+            } else {
+                await interaction.reply(`That spook is already on the syllabus!`);
+            }
         } else if (movieYear.length !== 4) { // YEAR CAN ONLY BE 4 CHARACTERS
             await interaction.reply(`That's not a real year......... I'm not adding that :(`);
         } else {
@@ -43,9 +66,9 @@ module.exports = {
                 title: `${movieTitle}`,
                 year: parseInt(movieYear),
                 director: `${movieDir}`,
-                seen: false
+                seen: seen
             });*/
-            await interaction.reply(`New spook added: ${movieTitle} (${movieYear})!`);
+            await interaction.reply(`New spook added: ${movieTitle} (${movieYear}) ${seen}!`);
         }
         await client.close();
     }
